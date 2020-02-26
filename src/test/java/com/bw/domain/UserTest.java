9b57a4970133f36dd2347c88b8a7e4f8b1e8062b
caohuaxing <chx1357262719@qq.com>
2020-02-26 10:20:39 +0800

    提交1

---
+++ src/test/java/com/bw/domain/UserTest.java
@@ -0,0 +1,15 @@
+package com.bw.domain;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class UserTest {
+
+	@Test
+	public void test1() {
+		
+		System.out.println("µ¥Ôª²âÊÔ£¡");
+	}
+	
+}
